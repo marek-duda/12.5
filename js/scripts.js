a5187c66cdff992bd325550e06746b9ff81b0b93
Maro <grafika.marekduda@gmail.com>
2017-06-26 00:29:14 +0200

    added css

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@ function createTweet(input) {
     var quoteAuthor = data.title; 
 
     if (!quoteAuthor.length) { 
-		quoteAuthor = "Unknown author";
+        quoteAuthor = "Unknown author";
     }
 
     var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
@@ -22,11 +22,11 @@ function createTweet(input) {
     if (tweetText.length > 140) { 
         getQuote(); 
 	} else { 
-        var tweet = tweetLink + encodeURIComponent(tweetText); 
-        $('.quote').text(quoteText); 
-        $('.author').text("Author: " + quoteAuthor); 
-        $('.tweet').attr('href', tweet); 
-	}
+          var tweet = tweetLink + encodeURIComponent(tweetText); 
+          $('.quote').text(quoteText); 
+          $('.author').text("Author: " + quoteAuthor); 
+          $('.tweet').attr('href', tweet); 
+    }
 }
 
 $(document).ready(function() { 
